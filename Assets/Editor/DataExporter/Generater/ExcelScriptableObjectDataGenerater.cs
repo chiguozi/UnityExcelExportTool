34bccec573b3fae62807f6eea0f416ec71ddc094
chiguozi <qdlzr456@163.com>
2017-05-30 22:44:57 +0800

    del

--- Assets/Editor/DataExporter/Generater/ExcelScriptableObjectDataGenerater.cs
+++ Assets/Editor/DataExporter/Generater/ExcelScriptableObjectDataGenerater.cs
@@ -18,12 +18,6 @@ public class ExcelScriptableObjectDataGenerater : IExcelDataGenerater
         AssetDatabase.CreateAsset(objContainer, Path.Combine(relativePath, fileName));
         for (int i = 0; i < data.cellList.Count; i++)
         {
-            //var field = objContainer.GetType().GetField("dataList", BindingFlags.Public | BindingFlags.Instance);
-            ////IList fieldValue = (IList)field.GetValue(objContainer);
-            ////fieldValue.Add(data.GetObject(i, type));
-            //var addMethod = field.FieldType.GetMethod("Add", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance);
-            //var tmp = field.GetValue(objContainer);
-            //addMethod.Invoke(field.GetValue(objContainer), new object[] { data.GetObject(i, type) });
             var dataInstance = data.GetSOObject(i, type) as ConfigSoBase;
             dataInstance.hideFlags = HideFlags.HideInInspector;
             objContainer.dataList.Add(dataInstance);
